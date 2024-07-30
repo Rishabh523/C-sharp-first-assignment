93bfce609370e29bac854a5e06f76e0a76677953
Rishabh Pratap Singh <116909258+Rishabh523@users.noreply.github.com>
2024-07-30 09:14:44 +0530

    Create Program to Check given no. is even or odd.cs

---
+++ Program to Check given no. is even or odd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n%2 == 0)
+        {
+            Console.WriteLine($"{n} is even.");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is odd.");
+        }
+        Console.ReadLine();
+    }
+}
