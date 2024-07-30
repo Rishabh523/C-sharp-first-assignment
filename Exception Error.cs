6bc694d2b7ebdda2382784c0d227ce01af6980be
Rishabh Pratap Singh <116909258+Rishabh523@users.noreply.github.com>
2024-07-30 09:13:09 +0530

    Create Exception Error.cs

---
+++ Exception Error.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ExceptionHandlingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_1, number_2, result;
+            try
+            {
+                Console.WriteLine("Enter First Number:");
+                number_1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number:");
+                number_2 = int.Parse(Console.ReadLine());
+                result = Number1 / Number2;
+                Console.WriteLine($"Result = {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Source: {ex.Source}");
+                Console.WriteLine($"HelpLink: {ex.HelpLink}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
