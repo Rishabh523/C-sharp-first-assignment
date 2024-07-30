91a3daf421ea3f3891cf7e3eed51b83413f15c15
Rishabh Pratap Singh <116909258+Rishabh523@users.noreply.github.com>
2024-07-30 09:20:04 +0530

    Create Program to check no. is greater than 1000.cs

---
+++ Program to check no. is greater than 1000.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a numeric integer: ");
+
+        try
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0 || num > 1000)
+            {
+                throw new Exception("Please enter the number between 0 and 1000.");
+            }
+
+            Console.WriteLine($"You entered: {num}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        Console.ReadLine();
+    }
+}
