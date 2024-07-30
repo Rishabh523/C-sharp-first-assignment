f48edebd3fba81a7167926c903f388be21550bac
Rishabh Pratap Singh <116909258+Rishabh523@users.noreply.github.com>
2024-07-30 09:21:38 +0530

    Create Words of a string.cs

---
+++ Words of a string.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a string:");
+        string input = Console.ReadLine();
+
+        // Split the string into words using spaces as the delimiter
+        string[] words = input.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Count the number of words
+        int word_Count = words.Length;
+
+        Console.WriteLine($"Total number of words: {word_Count}");
+        Console.ReadLine();
+    }
+    
+}
